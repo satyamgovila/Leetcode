e79f0fa99bdc6e270db28153661fa9a3be686e66
Satyam Govila <satyamgovilla007@gmail.com>
2021-08-19 22:36:25 +0530

    Update Reverse_Integer.py

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -13,3 +13,22 @@ class Solution:
           return -1 * output
         else:
           return output
+    
+    
+    
+ #####################################################################
+  class Solution:
+    def reverse(self, x: int) -> int:
+        sum=0
+        sign=1
+        num = x
+        if num<0:
+            sign=-1
+            num=num*-1
+        while num>0:
+            rem=num%10
+            sum=sum*10+rem
+            num=num//10
+        if not -2147483648<sum<2147483647:
+            return 0
+        return sign*sum
