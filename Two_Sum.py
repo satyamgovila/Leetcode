3c59212372c736f6a0b4b2b2642ce52eb221543e
Satyam Govila <satyamgovilla007@gmail.com>
2021-08-19 22:01:46 +0530

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,3 +1,6 @@
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_table = {} # key  = nums[i] , value = index of nums[i]
