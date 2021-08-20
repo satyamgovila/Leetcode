634405a61ec173ed91aba4d33d1abadaf2731e40
Satyam Govila <satyamgovilla007@gmail.com>
2021-08-20 22:20:55 +0530

    Update Reverse_Linked_List.py

--- Reverse_Linked_List.py
+++ Reverse_Linked_List.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+## Iterative Solution
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev  = None
@@ -15,4 +17,26 @@ class Solution:
             current = next
         self.head  = prev
         return prev
+    
+   
+
+
+## Recursive Solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.reverse_helper(head)
+    
+    def reverse_helper(self , node , prev = None):
+        if not node:
+            return prev
+        n = node.next
+        node.next = prev
+        
+        return self.reverse_helper(n , node)
         
