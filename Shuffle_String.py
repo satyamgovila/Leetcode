7485107616be0bc0e4c29b1dd5883aa73e911d5a
Satyam Govila <satyamgovilla007@gmail.com>
2021-08-24 12:46:18 +0530

    Update Shuffle_String.py

--- Shuffle_String.py
+++ Shuffle_String.py
@@ -1,8 +1,4 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        result = [x for x in sorted(zip(indices,[char for char in s]))]
-        ans = ""
-        for res in result:
-            ans+= res[1]
-        return ans
+        return "".join(x for _,x in sorted(zip(indices,[char for char in s])))
         
