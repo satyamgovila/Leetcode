16840f908c2afc9b3ddef1de5b13d552b88556a2
Satyam Govila <satyamgovilla007@gmail.com>
2021-08-30 13:41:34 +0530

    Create Replace Words.py

---
+++ Replace Words.py
@@ -0,0 +1,9 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        sentence = sentence.split()        
+        for root in dictionary:
+            for i, word in list(enumerate(sentence)):
+                if word.startswith(root):
+                    sentence[i] = root        
+        return " ".join(c for c in sentence)
+        
