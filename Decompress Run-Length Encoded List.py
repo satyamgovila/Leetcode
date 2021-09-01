5612ec179d1da91b62040e521dc9be9f2b5860a5
Satyam Govila <satyamgovilla007@gmail.com>
2021-09-01 10:28:24 +0530

    Create Decompress Run-Length Encoded List.py

---
+++ Decompress Run-Length Encoded List.py
@@ -0,0 +1,9 @@
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        res = []
+        for i in range(0 ,len(nums)-1 , 2):
+            freq = nums[i]
+            val = nums[i+1]
+            res.extend([val]*freq)
+        return res
+        
